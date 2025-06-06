5b92cc52afc8114986bd1e189b892bde2cb8815e
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-06 10:55:04 +0200

    Update ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -11,9 +11,13 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
-#include <fcntl.h>
 
+/**
+ * @brief Outputs the character c to the given file descriptor.
+ *
+ * @param c The character to output.
+ * @param fd The file descriptor on which to write.
+ */
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
